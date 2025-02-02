b1c19e38edee99382a2a547de3fa6e8c02c11d3f
Rexx Alfaro <rexxalfaro+github@gmail.com>
2025-02-01 18:03:43 -0800

    Added select element for status and implemented dialog and modal to the book icon

--- main.js
+++ main.js
@@ -1,6 +1,18 @@
 const myLibrary = [];
 const container = document.querySelector('.container')
 
+// Show dialog modal
+const modal = document.querySelector('.modal');
+const openModal = document.querySelector('.header button');
+const closeModal = document.querySelector('.close');
+
+openModal.addEventListener('click', () => {
+    modal.showModal();
+})
+closeModal.addEventListener('click', () => {
+    modal.close();
+})
+
 function Book(title,author,pages) {
     this.title = title;
     this.author = author;
@@ -24,6 +36,25 @@ function displayBooks() {
         const author = document.createElement('h3');
         const pages = document.createElement('h6');
 
+        // Create select element for status
+        const select =  document.createElement('select');
+        select.id = 'status';
+
+        // Define the options in an array of objects
+        const options = [
+            {value: 'status', text: '--Status--'},
+            {value: 'reading', text: 'Reading'},
+            {value: 'on-hold', text: 'On-Hold'},
+            {value: 'completed', text: 'Completed'},
+        ]
+
+        //Loop through the array and create <option> elements
+        options.forEach(items => {
+            const option = document.createElement('option');
+            option.value = items.value;
+            option.textContent = items.text;
+            select.appendChild(option);
+        })
         // Set text content for each element
         title.textContent = items.title;
         author.textContent = items.author;
@@ -32,10 +63,13 @@ function displayBooks() {
         // Append each element to the card
         newCard.appendChild(title);
         newCard.appendChild(author);
+        newCard.appendChild(select);
         newCard.appendChild(pages);
         
     })
 }
+
+
 
 addBookToLibrary('The Fable', 'Rexx', 12);
 addBookToLibrary('The GTO', 'Sarah', 10);
